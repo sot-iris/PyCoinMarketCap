83ba57df5627e9c2646b4a0f76edd2b06597ec1a
sot-iris <skakanos@hotmail.co.uk>
2017-12-31 22:57:00 +0000

    removed round function

--- cryptomarketcap.py
+++ cryptomarketcap.py
@@ -1,13 +1,13 @@
 import requests
 
 class Crypto:
-    def __init__(self, id, price_in, sigfig=2):
+    def __init__(self, id, price_in):
         self.id = id.title()
         self.price_in = price_in
         self.getinfo = requests.get("https://api.coinmarketcap.com/v1/ticker/%s/?convert=%s" % (self.id, self.price_in))
         firstindex = self.getinfo.json()
         self.info = firstindex[0]
-        self.price_usd = round(float(self.info["price_usd"]), sigfig)
-        self.price_gbp = round(float(self.info["price_gbp"]), sigfig)
-        self.market_cap_usd = round(float(self.info["market_cap_usd"]), sigfig)
+        self.price_usd = float(self.info["price_usd"])
+        self.price_gbp = float(self.info["price_gbp"])
+        self.market_cap_usd = float(self.info["market_cap_usd"])
 
