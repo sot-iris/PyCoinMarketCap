0b14278c79d1a45e2676fc0b953e0005ecc2b82f
sot-iris <skakanos@hotmail.co.uk>
2018-01-01 00:13:24 +0000

    Made less GBP centric

--- Examples/port_tracker.py
+++ Examples/port_tracker.py
@@ -4,9 +4,11 @@ from cryptomarketcap import Crypto
 
 #Portfolio tracker
 #Declare cryptoassets in this dictionary along with the number owned
-portfolio={"bitcoin": 100, "ethereum": 2000, "litecoin": 80, "iota": 1900, "ripple": 20000, "cardano": 20000}
 
+portfolio={"bitcoin": 100, "ethereum": 2000, "litecoin": 80, "iota": 1900, "ripple": 20000, "cardano": 20000}
 currency = "GBP"
+
+
 total = 0
 
 #this for-loop iterates through the keys and values in the portfolio dictionary
@@ -15,9 +17,9 @@ for key, value in portfolio.items():
     #create each cryptocurrency object from which to gather their attributes
     #this will return the cryptoasset prices in GBP
     coin = Crypto(key, currency)
-    v = value*coin.price_gbp
+    v = value*coin.price
     total += v
-    print("%s: %s GBP" % (key.title(), v))
+    print("%s: %s %s" % (key.title(), v, currency))
 
 print("Your total holdings: %s in %s" % (total, currency))
 
--- cryptomarketcap.py
+++ cryptomarketcap.py
@@ -2,12 +2,11 @@ import requests
 
 class Crypto:
     def __init__(self, id, price_in):
+        getinfo = requests.get("https://api.coinmarketcap.com/v1/ticker/%s/?convert=%s" % (id, price_in))
+        firstindex = getinfo.json()
         self.id = id.title()
-        self.price_in = price_in
-        self.getinfo = requests.get("https://api.coinmarketcap.com/v1/ticker/%s/?convert=%s" % (self.id, self.price_in))
-        firstindex = self.getinfo.json()
         self.info = firstindex[0]
         self.price_usd = float(self.info["price_usd"])
-        self.price_gbp = float(self.info["price_gbp"])
+        self.price = float(self.info["price_%s" % (price_in.lower())])
         self.market_cap_usd = float(self.info["market_cap_usd"])
 
